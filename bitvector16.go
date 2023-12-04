a86658f4834c477ff62454ed4380121ec223ea90
cyyber <kaushal.forex@gmail.com>
2023-12-04 14:11:43 +0530

    Implemented Or, Overlaps, Contains and Shift function for bitvector16

--- bitvector16.go
+++ bitvector16.go
@@ -1,6 +1,7 @@
 package bitfield
 
 import (
+	"encoding/binary"
 	"math/bits"
 )
 
@@ -79,6 +80,29 @@ func (b Bitvector16) Bytes() []byte {
 	return ret[:]
 }
 
+// Shift bitvector by i. If i >= 0, perform left shift, otherwise right shift.
+func (b Bitvector16) Shift(i int) {
+	if len(b) == 0 {
+		return
+	}
+
+	// Shifting greater than 16 bits is pointless and can have unexpected behavior.
+	if i > bitvector16BitSize {
+		i = bitvector16BitSize
+	} else if i < -bitvector16BitSize {
+		i = -bitvector16BitSize
+	}
+	if i >= 0 {
+		num := binary.BigEndian.Uint64(b)
+		num <<= uint8(i)
+		binary.BigEndian.PutUint64(b, num)
+	} else {
+		num := binary.BigEndian.Uint64(b)
+		num >>= uint8(i * -1)
+		binary.BigEndian.PutUint64(b, num)
+	}
+}
+
 // BitIndices returns the list of indices which are set to 1.
 func (b Bitvector16) BitIndices() []int {
 	indices := make([]int, 0, bitvector16BitSize)
@@ -95,4 +119,63 @@ func (b Bitvector16) BitIndices() []int {
 	}
 
 	return indices
+}
+
+// Contains returns true if the bitlist contains all of the bits from the provided argument
+// bitlist. This method will return an error if bitlists are not the same length.
+func (b Bitvector16) Contains(c Bitvector16) (bool, error) {
+	if b.Len() != c.Len() {
+		return false, ErrBitvectorDifferentLength
+	}
+
+	// To ensure all of the bits in c are present in b, we iterate over every byte, combine
+	// the byte from b and c, then XOR them against b. If the result of this is non-zero, then we
+	// are assured that a byte in c had bits not present in b.
+	for i := 0; i < len(b); i++ {
+		if b[i]^(b[i]|c[i]) != 0 {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
+// Overlaps returns true if the bitlist contains one of the bits from the provided argument
+// bitlist. This method will return an error if bitlists are not the same length.
+func (b Bitvector16) Overlaps(c Bitvector16) (bool, error) {
+	lenB, lenC := b.Len(), c.Len()
+	if b.Len() != c.Len() {
+		return false, ErrBitvectorDifferentLength
+	}
+
+	if lenB == 0 || lenC == 0 {
+		return false, nil
+	}
+
+	// To ensure all of the bits in c are not overlapped in b, we iterate over every byte, invert b
+	// and xor the byte from b and c, then and it against c. If the result is non-zero, then
+	// we can be assured that byte in c had bits not overlapped in b.
+	for i := 0; i < len(b); i++ {
+		// If this byte is the last byte in the array, mask the length bit.
+		mask := uint8(0xFF)
+
+		if (^b[i]^c[i])&c[i]&mask != 0 {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// Or returns the OR result of the two bitfields. This method will return an error if the bitlists are not the same length.
+func (b Bitvector16) Or(c Bitvector16) (Bitvector16, error) {
+	if b.Len() != c.Len() {
+		return nil, ErrBitvectorDifferentLength
+	}
+
+	ret := make([]byte, len(b))
+	for i := 0; i < len(b); i++ {
+		ret[i] = b[i] | c[i]
+	}
+
+	return ret, nil
 }
